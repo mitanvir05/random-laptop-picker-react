9f5070b5d6c7338815fe4829b40887a9bba017e9
mitanvir05 <tanvir.cse05@gmail.com>
2022-03-26 12:38:22 +0600

    card

--- src/card/Card.js
+++ src/card/Card.js
@@ -1,15 +1,13 @@
 import React from "react";
 import"./Card.css";
 const Card = ({gunData}) => {
-    //console.log(props.gunData);
     const {name} =gunData;
-      //console.log(name);
   
     return(
 
         <div className="card">
             <div>
-                
+
             </div>
             <h1>Card</h1>
         </div>
