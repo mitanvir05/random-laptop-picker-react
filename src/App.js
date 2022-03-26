d006361b559c44b2046d484ef139f022ffaa6ed0
mitanvir05 <tanvir.cse05@gmail.com>
2022-03-26 12:33:26 +0600

    card added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './card/Card';
 
 function App() {
+  const [guns , setGuns]=useState([]);
+useEffect(() => {
+  fetch("data.json")
+  .then((res) => res.json())
+  .then((data) => setGuns(data));
+},[]);
+
   return (
-    <div className="App">
+    <div>
+      <h1>Random Gun Picker</h1>
       
+      {
+        guns.map(gun => <Card key={gun.id} gunData ={gun}/>)
+      }
     </div>
   );
 }
---
+++ src/card/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+import"./Card.css";
+const Card = ({gunData}) => {
+    //console.log(props.gunData);
+    const {name} =gunData;
+      //console.log(name);
+  
+    return(
+
+        <div className="card">
+            <div>
+                
+            </div>
+            <h1>Card</h1>
+        </div>
+    )
+};
+
+export default Card;
